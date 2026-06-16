f955752392aeb529026d8b38394eee6812a3729e
onlyhereforblackeye <iakobiaxvlediani0@gmail.com>
2026-06-16 15:22:42 -0700

    Implement WebView in MainActivity

--- app/src/main/java/com/webviewapp/MainActivity.kt
+++ app/src/main/java/com/webviewapp/MainActivity.kt
@@ -1,1 +1,17 @@
+package com.webviewapp
 
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        
+        val webView: WebView = findViewById(R.id.webView)
+        webView.webViewClient = WebViewClient()
+        webView.loadUrl("{{APP_URL}}")
+    }
+}
